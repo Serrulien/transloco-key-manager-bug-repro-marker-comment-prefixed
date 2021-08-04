d1b330e1acf23a7a4fed4ef4f59f307af1b2960e
Julien Serrurier <contact@julienserrurier.fr>
2021-08-04 14:54:07 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
+/**
+ * t(OK) // should be detected as a marker
+ * combineLastest(ERR) // should NOT be detected as a marker
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'transloco-key-manager-bug-repro-marker-comment-prefixed';
